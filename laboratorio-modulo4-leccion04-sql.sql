2e64d6e4f3c73eb1b62b750e1c582ab606af1f4c
apelsito <mrappletastico@gmail.com>
2024-10-24 21:25:18 +0200

    Hasta aqui hemos llegao

--- laboratorio-modulo4-leccion04-sql.sql
+++ laboratorio-modulo4-leccion04-sql.sql
@@ -82,19 +82,14 @@ SELECT *
 FROM localesconcodigopostal
 
 -- 2.2. Crea una vista con los locales que tienen más de una categoría asociada.
-CREATE VIEW localesquetienenmasdeunacategoria AS
+CREATE VIEW LocalesConMasDeUnaCategoria AS
 SELECT l.nombre AS lugar, COUNT(l.id_categoria) AS categorias_asociadas
 FROM lugares l
-INNER JOIN categorias c 
-ON c.id_categoria = l.id_categoria 
 GROUP BY l.nombre
 HAVING COUNT(l.id_categoria) > 1
 ORDER BY l.nombre;
 
-
-SELECT * FROM localesquetienenmasdeunacategoria 
---Ta mal 
-
+SELECT * FROM LocalesConMasDeUnaCategoria
 
 -- 2.3. Crea una vista que muestre el municipio con la temperatura más alta de cada día
 CREATE VIEW municipioTempMasAlta AS
@@ -108,53 +103,145 @@ SELECT fecha,MAX(temperatura_maxima)
 	ON m.id_municipio = t.id_municipio 
 	GROUP BY f.fecha::DATE,m.id_municipio 
 	ORDER BY fecha)
-WHERE municipio IN (SELECT nombre 
-					FROM municipios m2)
 GROUP BY fecha
 
 SELECT * FROM municipioTempMasAlta
 
 -- NO SE HACERLO me rindo
 
--- 2.4. Crea una vista con los municipios en los que haya una probabilidad de precipitación mayor del 100% durante mínimo 7 horas.
-SELECT COUNT(f.fecha) 
-FROM tiempo t 
-INNER JOIN fechas f
-ON f.id_fecha = t.id_fecha 
-WHERE probabilidad_de_tormenta > 80
+-- 2.4. Crea una vista con los municipios en los que haya una probabilidad de precipitación mayor del 70% durante mínimo 7 horas.
+CREATE VIEW precipitacion70masde7horas AS
+	SELECT m.nombre AS municipio, COUNT(t.id_tiempo) AS horas_probabilidad
+	FROM tiempo t
+	INNER JOIN fechas f ON t.id_fecha = f.id_fecha
+	INNER JOIN municipios m ON t.id_municipio = m.id_municipio
+	WHERE t.probabilidad_de_tormenta > 70
+	GROUP BY m.nombre
+	HAVING COUNT(t.id_tiempo) >= 7
+	ORDER BY horas_probabilidad DESC;
+
+SELECT * FROM precipitacion70masde7horas
 
 
 -- 2.5. Obtén una lista con los parques de los municipios que tengan algún castillo.
+CREATE VIEW parquesquecontienencastillo AS
+	SELECT l.nombre AS parque, m.nombre AS municipio
+	FROM lugares l
+	INNER JOIN municipios m ON l.id_municipio = m.id_municipio
+	INNER JOIN categorias c ON l.id_categoria = c.id_categoria
+	WHERE (c.nombre = 'Park' OR c.nombre = 'Castle') AND l.nombre LIKE '%astillo%'
 
+SELECT * FROM parquesquecontienencastillo
 -- ## Ejercicio 3. Tablas Temporales
 
 -- 3.1. Crea una tabla temporal que muestre cuántos días han pasado desde que se obtuvo la información de la tabla AEMET.
+CREATE TEMPORARY TABLE diastranscurridos AS
+SELECT DISTINCT f.fecha::DATE AS fecha_obtencion,
+       CURRENT_DATE - f.fecha::DATE AS dias_transcurridos
+FROM fechas f
+ORDER BY dias_transcurridos DESC
 
+SELECT * FROM diastranscurridos
 -- 3.2. Crea una tabla temporal que muestre los locales que tienen más de una categoría asociada e indica el conteo de las mismas
+ 
+CREATE TEMPORARY TABLE localescategorias AS
+SELECT l.nombre AS lugar, COUNT(l.id_categoria) AS cantidad_categorias
+FROM lugares l
+GROUP BY l.nombre
+HAVING COUNT(l.id_categoria) > 1;
 
+SELECT * FROM localescategorias
 -- 3.3. Crea una tabla temporal que muestre los tipos de cielo para los cuales la probabilidad de precipitación mínima de los promedios de cada día es 5.
+
 
 -- 3.4. Crea una tabla temporal que muestre el tipo de cielo más y menos repetido por municipio.
 
 -- ## Ejercicio 4. SUBQUERIES
 
 --4.1. Necesitamos comprobar si hay algún municipio en el cual no tenga ningún local registrado.
+SELECT m.nombre AS municipio
+FROM municipios m
+WHERE m.id_municipio NOT IN (
+    SELECT DISTINCT l.id_municipio
+    FROM lugares l);
 
 --4.2. Averigua si hay alguna fecha en la que el cielo se encuente "Muy nuboso con tormenta".
 
---4.3. Encuentra los días en los que los avisos sean diferentes a "Sin riesgo".
+SELECT * 
+FROM (
+		SELECT f.fecha::DATE,m.nombre AS Municipio,ec.nombre AS estado_cielo
+		FROM tiempo t
+		INNER JOIN fechas f 
+		ON f.id_fecha = t.id_fecha 
+		INNER JOIN estado_cielo ec 
+		ON t.id_estado_cielo = ec.id_estado_cielo
+		INNER JOIN municipios m
+		ON t.id_municipio = m.id_municipio)
+WHERE estado_cielo = 'Muy nuboso con tormenta'
+
+--4.3. Encuentra los días en los que el cielo sea diferente a "Muy nuboso con tormenta".
+
+SELECT * 
+FROM (
+	SELECT f.fecha::DATE,m.nombre AS Municipio,ec.nombre AS estado_cielo
+	FROM tiempo t
+	INNER JOIN fechas f 
+	ON f.id_fecha = t.id_fecha 
+	INNER JOIN estado_cielo ec 
+	ON t.id_estado_cielo = ec.id_estado_cielo
+	INNER JOIN municipios m
+	ON t.id_municipio = m.id_municipio
+)
+WHERE estado_cielo != 'Muy nuboso con tormenta';
 
 --4.4. Selecciona el municipio con mayor número de locales.
+
+SELECT * 
+FROM (
+	SELECT m.nombre AS municipio, COUNT(l.id_lugar) AS numero_locales
+	FROM municipios m
+	INNER JOIN lugares l ON m.id_municipio = l.id_municipio
+	GROUP BY municipio)
+ORDER BY numero_locales DESC
+LIMIT 1;
 
 --4.5. Obtén los municipios muya media de sensación térmica sea mayor que la media total.
 
 --4.6. Selecciona los municipios con más de dos fuentes.
+SELECT * 	
+FROM (
+	SELECT DISTINCT c.nombre AS categoria, m.nombre AS municipio, COUNT(c.nombre = 'Fountain') AS numero_fuentes
+	FROM lugares l 
+	INNER JOIN categorias c 
+	ON l.id_categoria = c.id_categoria 
+	INNER JOIN municipios m 
+	ON l.id_municipio = m.id_municipio 
+	GROUP BY c.nombre, m.nombre ) 
+WHERE categoria = 'Fountain';
 
 --4.7. Localiza la dirección de todos los estudios de cine que estén abiertod en el municipio de "Madrid".
+SELECT * 	
+FROM (
+	SELECT m. nombre AS municipio,l.nombre AS local, c.nombre AS categoria
+	FROM lugares l 
+	INNER JOIN categorias c 
+	ON l.id_categoria = c.id_categoria 
+	INNER JOIN municipios m 
+	ON l.id_municipio = m.id_municipio)
+WHERE municipio = 'Madrid' AND categoria = 'Film Studio';
 
 --4.8. Encuentra la máxima temperatura para cada tipo de cielo.
+SELECT * 
+FROM(
+	SELECT  ec.nombre AS estado_cielo ,MAX(t.temperatura) AS temperatura_maxima
+	FROM tiempo t
+	NATURAL JOIN estado_cielo ec 
+	GROUP BY ec.nombre )
+ORDER BY temperatura_maxima DESC
 
 --4.9. Muestra el número de locales por categoría que muy probablemente se encuentren abiertos.
+
+SELECT * FROM lugares l 
 
 --BONUS. 4.10. Encuentra los municipios que tengan más de 3 parques, los cuales se encuentren a una distancia menor 
 --de las coordenadas de su municipio correspondiente que la del Parque Pavia. 
